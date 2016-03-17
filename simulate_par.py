2a93791b4244113c7a8e0fa4a2082c085982b0a7
andrenmelo <andre.nascimento.melo@gmail.com>
2016-03-17 14:24:46 +0100

    joined for i,j loops into one line using product, fixed energy function

--- simulate_par.py
+++ simulate_par.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 from scipy.ndimage import convolve
-import random
 import math
+import itertools
 from numba import jit
 
 #   Simulation parameters
@@ -24,70 +24,65 @@ links_hk= np.zeros([N, N, 2])
 @jit
 def link(N, lattice_hk, links_hk, betaJ):
     prob = np.exp(-2*betaJ)
-    for i in range (0, N):
-        for j in range (0, N):
-            if lattice_hk[i,j] == lattice_hk[(i-1)%N, j]:
-                if np.random.uniform(0,1) < prob:
-                    links_hk[i,j,0] = 0
-                else:
-                    links_hk[i,j,0] = 1
+    for i, j in itertools.product(range(N), range(N)):
+        if lattice_hk[i,j] == lattice_hk[(i-1)%N, j]:
+            if np.random.uniform(0,1) < prob:
+                links_hk[i,j,0] = 0
+            else:
+                links_hk[i,j,0] = 1
 
-            if lattice_hk[i,j] == lattice_hk[i, (j-1)%N]:
-                if np.random.uniform(0,1) < prob:
-                    links_hk[i,j,1] = 0
-                else:
-                    links_hk[i,j,1] = 1
+        if lattice_hk[i,j] == lattice_hk[i, (j-1)%N]:
+            if np.random.uniform(0,1) < prob:
+                links_hk[i,j,1] = 0
+            else:
+                links_hk[i,j,1] = 1
     return links_hk
 
 @jit
 def latti_upd(N, lattice_hk, links_hk, betaJ):
     largest_label = 0 
     label_hk = np.zeros([N, N])
-    for i in range (0, N):
-        for j in range (0, N):         
-            if links_hk[i,j,0] == 1 and links_hk[i,j,1] != 1:                        #can be done in one if
-                label_hk[i,j] = label_hk[(i-1)%N, j]
-            if links_hk[i,j,1] == 1 and links_hk[i,j,0] != 1:
-                label_hk[i,j] = label_hk[i, (j-1)%N]
-            elif links_hk[i,j,1] == 1 and links_hk[i,j,0] == 1:
-                label_hk[i,j] = label_hk[(i-1)%N, j]
-            elif all(links_hk[i,j,:]) != 1:
-                largest_label += 1
-                label_hk[i,j] = largest_label
+    for i, j in itertools.product(range(N), range(N)):
+        if links_hk[i,j,0] == 1 and links_hk[i,j,1] != 1:                        #can be done in one if
+            label_hk[i,j] = label_hk[(i-1)%N, j]
+        if links_hk[i,j,1] == 1 and links_hk[i,j,0] != 1:
+            label_hk[i,j] = label_hk[i, (j-1)%N]
+        elif links_hk[i,j,1] == 1 and links_hk[i,j,0] == 1:
+            label_hk[i,j] = label_hk[(i-1)%N, j]
+        elif all(links_hk[i,j,:]) != 1:
+            largest_label += 1
+            label_hk[i,j] = largest_label
     print("----")
     print(largest_label)
-    for i in range (0, N):
-        for j in range (0, N):
-            if links_hk[i,j,0] ==1 and label_hk[i,j] != label_hk[(i-1)%N, j]:
-                label_hk[i, j] = min(label_hk[i,j], label_hk[(i-1)%N, j])
-                label_hk[(i-1)%N, j] = min(label_hk[i,j], label_hk[(i-1)%N, j])
-            if links_hk[i,j,1] ==1 and label_hk[i,j] != label_hk[i, (j-1)%N]:
-                label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j-1)%N])
-                label_hk[i, (j-1)%N] = min(label_hk[i,j], label_hk[i, (j-1)%N])
-            if links_hk[(i+1)%N,j,0] ==1 and label_hk[i,j] != label_hk[(i+1)%N, j]:
-                label_hk[i, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
-                label_hk[(i+1)%N, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
-            if links_hk[i,(j+1)%N,1] ==1 and label_hk[i,j] != label_hk[i, (j+1)%N]:
-                label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j+1)%N])
-                label_hk[i, (j+1)%N] = min(label_hk[i,j], label_hk[i, (j+1)%N])
+    for i, j in itertools.product(range(N), range(N)):
+        if links_hk[i,j,0] ==1 and label_hk[i,j] != label_hk[(i-1)%N, j]:
+            label_hk[i, j] = min(label_hk[i,j], label_hk[(i-1)%N, j])
+            label_hk[(i-1)%N, j] = min(label_hk[i,j], label_hk[(i-1)%N, j])
+        if links_hk[i,j,1] ==1 and label_hk[i,j] != label_hk[i, (j-1)%N]:
+            label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j-1)%N])
+            label_hk[i, (j-1)%N] = min(label_hk[i,j], label_hk[i, (j-1)%N])
+        if links_hk[(i+1)%N,j,0] ==1 and label_hk[i,j] != label_hk[(i+1)%N, j]:
+            label_hk[i, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
+            label_hk[(i+1)%N, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
+        if links_hk[i,(j+1)%N,1] ==1 and label_hk[i,j] != label_hk[i, (j+1)%N]:
+            label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j+1)%N])
+            label_hk[i, (j+1)%N] = min(label_hk[i,j], label_hk[i, (j+1)%N])
 
     largest_label = largest_label +1
     up_lattice = np.zeros([N, N])
     new_spin = np.random.choice([1, -1], size = largest_label)
 
-    for i in range (0, N):
-        for j in range(0, N):
-            up_lattice[i, j] = new_spin[(label_hk[i,j])]
+    for i, j in itertools.product(range(N), range(N)):
+        up_lattice[i, j] = new_spin[(label_hk[i,j])]
+    
     return up_lattice
 
-@jit
-def energy_cal(lattice_hk, betaJ):
-    E = 0
+def compute_energy(lattice):
     k = np.array([[0,1,0],[1,0,1],[0,1,0]])
-    for i in range (0,N):
-        for j in range (0,N):
-            E = E-0.5*betaJ*np.sum(lattice*convolve(lattice_hk, k, mode='wrap'))
-    return E
+    neighbour_sum = convolve(lattice, k, mode='wrap')
+    energy = -0.5*(np.sum(neighbour_sum*lattice))
+
+    return energy
 
 lattice_sum = np.zeros(100)
 x = np.zeros(100)
@@ -108,7 +103,7 @@ for i in range (0, n_iter):
 plt.plot(x,magnetization)
 plt.show()
     # # if i > n_iter_init:
-    # #     E[i-n_iter_init] = energy_cal(lattice_hk, betaJ)/N
+    # #     E[i-n_iter_init] = compute_energy(lattice_hk)
     # # print("-------")
     # print(i)
 
