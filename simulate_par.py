b76783db8ece2cdbf1e3e4848070f857e845f6e0
MatteoDome <mdomenighini@gmail.com>
2016-03-17 10:20:31 +0100

    working susceptibility

--- simulate_par.py
+++ simulate_par.py
@@ -7,61 +7,14 @@ import math
 from numba import jit
 
 #   Simulation parameters
-N = 50
+N = 10
 T = 1
 n_iter = 10000
 n_iter_init = 1000
 betaJ = 0.01
 E= np.zeros([n_iter - n_iter_init])
-#   System variables
-#lattice = np.ones([N, N])
-
-
-# #   Checkerboard pattern to flip spins
-# indices = np.full((N, N), True, dtype=bool)
-# indices[1::2,::2] = False
-# indices[::2,1::2] = False
-
-# #   This is used for the spin sum
-# k = np.array([[0,1,0],[1,0,1],[0,1,0]])
-
-# plt.ion()
-# fig = plt.figure()
-
-# magnetization = np.zeros(1000)
-# x = np.zeros(1000)
-
-# for i in range(n_iter):
-#     delta_energy = 2*betaJ*lattice*convolve(lattice, k, mode='wrap')  
-#     flipping_probability = np.exp(-delta_energy)-np.random.rand(N, N)
-
-#     #   Clean
-#     if i%2==0:
-#         flipping_probability[indices] = 0
-#     else:
-#         flipping_probability[~indices] = 0
-
-#     lattice[flipping_probability>0]=-lattice[flipping_probability>0]
-#     # if(betaJ<0.5):
-#     #     fig.clf()
-#     #     ax = fig.add_subplot(111)
-#     #     ax.matshow(lattice)
-#     #     plt.draw()
-
-#     if i%100==0:
-#         x[int(i/100)] = betaJ
-#         magnetization[int(i/100)] = np.mean(lattice)
-#         betaJ-=0.01
-#         print("BETAJ " + str(betaJ))
-
-#     # print(i)
-
-# plt.plot(x,magnetization)
-# plt.show()
-
 
 ##Hoshen Kopelman
-
 
 lattice_hk = np.zeros([N,N])
 for i in range (0,N):
@@ -88,42 +41,8 @@ def link(N, lattice_hk, links_hk, betaJ):
                 else:
                     links_hk[i,j,1] = 1
     return links_hk
-# @jit
-# def label(N, lattice_hk, links_hk, betaJ):
-#     largest_label = 0 
-#     label_hk = np.zeros([N, N])
-#     for i in range (0, N):
-#         for j in range (0, N):         
-#             if links_hk[i,j,0] == 1:                        #can be done in one if
-#                 label_hk[i,j] = label_hk[(i-1)%N, j]
-#             if links_hk[i,j,1] == 1:
-#                 label_hk[i,j] = label_hk[i, (j-1)%N]
-#             elif all(links_hk[i,j,:]) != 1:
-#                 largest_label = largest_label + 1
-#                 label_hk[i,j] = largest_label
 
-#     for i in range (0, N):
-#         for j in range (0, N):
-#             if links_hk[(i+1)%N,j,0] ==1 and label_hk[i,j] != label_hk[(i+1)%N, j]:
-#                 label_hk[i, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
-#             if links_hk[i,(j+1)%N,1] ==1 and label_hk[i,j] != label_hk[i, (j+1)%N]:
-#                 label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j+1)%N])
-
-#     return label_hk, largest_label
-
-# @jit
-# def new_lattice(N, lattice_hk, label_hk, largest_label):
-
-#     up_lattice = np.zeros([N, N])
-#     new_spin = np.zeros([largest_label])
-#     for i in range(0, largest_label):
-#         new_spin[i] = random.choice([-1, 1])
-#     for i in range (0, N):
-#         for j in range(0, N):
-#             up_lattice[i, j] = new_spin[(label_hk[i,j])]
-#     return up_lattice
-
-
+@jit
 def latti_upd(N, lattice_hk, links_hk, betaJ):
     largest_label = 0 
     label_hk = np.zeros([N, N])
@@ -142,10 +61,18 @@ def latti_upd(N, lattice_hk, links_hk, betaJ):
     print(largest_label)
     for i in range (0, N):
         for j in range (0, N):
+            if links_hk[i,j,0] ==1 and label_hk[i,j] != label_hk[(i-1)%N, j]:
+                label_hk[i, j] = min(label_hk[i,j], label_hk[(i-1)%N, j])
+                label_hk[(i-1)%N, j] = min(label_hk[i,j], label_hk[(i-1)%N, j])
+            if links_hk[i,j,1] ==1 and label_hk[i,j] != label_hk[i, (j-1)%N]:
+                label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j-1)%N])
+                label_hk[i, (j-1)%N] = min(label_hk[i,j], label_hk[i, (j-1)%N])
             if links_hk[(i+1)%N,j,0] ==1 and label_hk[i,j] != label_hk[(i+1)%N, j]:
                 label_hk[i, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
+                label_hk[(i+1)%N, j] = min(label_hk[i,j], label_hk[(i+1)%N, j])
             if links_hk[i,(j+1)%N,1] ==1 and label_hk[i,j] != label_hk[i, (j+1)%N]:
                 label_hk[i, j] = min(label_hk[i,j], label_hk[i, (j+1)%N])
+                label_hk[i, (j+1)%N] = min(label_hk[i,j], label_hk[i, (j+1)%N])
 
     largest_label = largest_label +1
     up_lattice = np.zeros([N, N])
@@ -166,16 +93,16 @@ def energy_cal(lattice_hk, betaJ):
             E = E-0.5*betaJ*np.sum(lattice*convolve(lattice_hk, k, mode='wrap'))
     return E
 
-lattice_sum = np.zeros(1000)
-x = np.zeros(1000)
-magnetization = np.zeros(1000)
+lattice_sum = np.zeros(100)
+x = np.zeros(100)
+magnetization = np.zeros(100)
 for i in range (0, n_iter):
     link(N, lattice_hk, links_hk, betaJ)
     lattice_hk = latti_upd(N, lattice_hk, links_hk, betaJ)
     lattice_sum[i%100] = lattice_hk.sum()
     if i%100==0:
         x[int(i/100)] = betaJ
-        magnetization[int(i/100)] = abs(np.mean(lattice_sum))
+        magnetization[int(i/100)] = abs(np.mean(lattice_sum*lattice_sum))/(N*N*N*N)
         betaJ+=0.01
         lattice_sum = np.zeros(100)
 
