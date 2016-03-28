2cf562201aa3d5aba33d6e6932962c25208d78f4
MatteoDome <mdomenighini@gmail.com>
2016-03-28 12:38:56 +0200

    code for plot now makes more sense

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-betaJ_values = np.arange(0.1, 1, 0.1)
+betaJ_values = np.arange(0.1, 1, 0.01)
 N_values = [4, 8, 16, 32, 64, 128, 256]
 
 with PdfPages('batch_results/magnetization.pdf') as pdf:
@@ -11,15 +11,22 @@ with PdfPages('batch_results/magnetization.pdf') as pdf:
     for N in N_values:
     	results = open("batch_results/"+str(N), 'rb')
     	object_results = pickle.load(results)
+    	magnetization = np.asarray(object_results[0])
     	for betaJ in betaJ_values:
-		    plt.plot(betaJ, object_results.magnetization[0], linewidth=2.0)
+    		betaJ_ind = int(round(betaJ*100 - 10))
+    		magn_pl = magnetization[betaJ_ind,1]
+    		print("*******")
+    		print("betaJ = " + str(betaJ))
+    		print("magnetization = " +str(magn_pl))
+    		print("N = "+str(N))
+    		plt.plot(betaJ, magn_pl, linewidth=2.0)
+    	results.close()
+	plt.xlabel('$betaJ$',fontsize=15)
+	plt.xlim([0, 1])
+	plt.ylabel('$magnetization$', fontsize=15)
+	plt.legend(loc=4)
+	plt.ylim([0,1])
+	pdf.savefig()
+    	
 
-		    plt.xlabel('$betaJ$',fontsize=15)
-		    plt.xlim([0, 1])
-		    plt.ylabel('$magnetization$', fontsize=15)
-		    plt.legend(loc=4)
-
-		    pdf.savefig() 
 plt.close()
-
-file.close()
