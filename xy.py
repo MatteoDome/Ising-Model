74f2695ae658f6af7d2d926fb667b3cf874db205
andrenmelo <andre.nascimento.melo@gmail.com>
2016-04-04 22:35:32 +0200

    fixed some things in xy, still not working though

--- xy.py
+++ xy.py
@@ -8,14 +8,13 @@ from numba import jit
 def find_links(N, spins, parallel_component, T):
     #   Compute coupling constants
     coupling = np.zeros([N, N, 2])
-    coupling[spins == np.roll(spins, 1, 0), 0]  = (np.abs(parallel_component)*np.abs(np.roll(parallel_component, 1, 0)))[spins == np.roll(spins, 1, 0)]
-    coupling[spins == np.roll(spins, 1, 1), 1]  = (np.abs(parallel_component)*np.abs(np.roll(parallel_component, 1, 1)))[spins == np.roll(spins, 1, 1)]
+    coupling[:, :, 0]  = (np.abs(parallel_component)*np.roll(np.abs(parallel_component), 1, 0))
+    coupling[:, :, 1]  = (np.abs(parallel_component)*np.roll(np.abs(parallel_component), 1, 1))
+
     prob = np.exp(-coupling/T)
 
-    #   Compute links
+    #   Set links to 1 if spins match
     links = np.zeros([N, N, 2], 'int_')
-
-    #   Set links to 1 if they match
     links[spins == np.roll(spins, 1, 0), 0] = 1
     links[spins == np.roll(spins, 1, 1), 1] = 1
 
@@ -24,7 +23,6 @@ def find_links(N, spins, parallel_component, T):
     links[(random_matrix < prob) & (links == 1)] = 0
 
     return links
-
 
 def canonical_label(label_list, label):
     while label != label_list[label]:
@@ -114,39 +112,38 @@ def compute_helicity_modulus(N, angles, T):
     a = np.sum(a, axis = (1, 2))/2
 
     b = np.sin(angles - np.roll(angles, 1, axis=2))
-    b = np.sum(b, axis = (1, 2))/2
-   
+    b = np.sum(b, axis = (1, 2))
+    
     c = np.sin(angles - np.roll(angles, 1, axis=1))
-    c = np.sum(c, axis = (1, 2))/2
-
+    c = np.sum(c, axis = (1, 2))
     helicity = 1/(2*N**2)*(np.mean(a) - np.mean(b**2)/T - np.mean(c**2)/T)
 
     return helicity
 
 def simulate(N, T_init, T_end, T_step, n_idle):
     #   Create random spins and normalize
-    lattice = np.random.uniform(-1, 1, size = [N, N, 2])
-    lattice = lattice/np.linalg.norm(lattice, axis=2, keepdims=True)    
+    # lattice = np.random.uniform(-1, 1, size = [N, N, 2])
+    # lattice = lattice/np.linalg.norm(lattice, axis=2, keepdims=True)   
+    lattice = np.ones(shape = [N, N, 2])/(np.sqrt(2)) 
     T = T_init
     label = np.zeros([N, N])
-    links = np.zeros([N, N, 2])
     n_iter = int((T_end - T_init) / T_step * n_idle)
     neighbour_list = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 
     #   Physical quantities to track
     helicity_modulus = []
     angles = np.zeros(shape = [n_idle, N, N])
-
+    
     #   Main cycle
     for i in range(n_iter):
-        #   Store angles to calculate helicity modulus
-        angles[int(i%n_idle), :, :] = lattice[:, :, 0]
+        #   Store x to calculate helicity modulus
+        angles[int(i%n_idle), :, :] = np.arctan(lattice[:, :, 1]/lattice[:, :, 0])
 
         #   Create random vector and normalize
         random_vec = np.random.uniform(-1, 1, size = 2)
         random_vec = random_vec/np.linalg.norm(random_vec)
 
-        #   Compute parallel and perpendicular components
+        #   Take inner product to compute parallel components
         parallel_component = np.sum(lattice*random_vec, axis=2)
 
         #   Create lattice of spin components along random vector
@@ -157,7 +154,7 @@ def simulate(N, T_init, T_end, T_step, n_idle):
         parallel_projection = parallel_component[:, :, None] * random_vec
         perpendicular_projection = lattice - parallel_projection
 
-        links = find_links(N, spins, parallel_component, 1000)
+        links = find_links(N, spins, parallel_component, T)
         cluster_labels, label_list, cluster_count = find_clusters(N, links)
         new_spins = assign_new_cluster_spins(N, cluster_labels, label_list)
 
@@ -165,9 +162,15 @@ def simulate(N, T_init, T_end, T_step, n_idle):
         parallel_projection[spins != new_spins, :] = - parallel_projection[spins != new_spins, :]
         lattice = parallel_projection + perpendicular_projection
 
-        if i % n_idle == 0:
-            helicity_modulus.append((T, compute_helicity_modulus(N, angles, T)))
+        # if i>400:
+        #     Q2 = plt.quiver(lattice[:, :, 0], lattice[:, :, 1])
+        #     l, r, b, t = plt.axis()
+        #     dx, dy = r - l, t - b
+        #     plt.axis([l - 0.05*dx, r + 0.05*dx, b - 0.05*dy, t + 0.05*dy])
+        #     plt.show()
 
+        if (i+1) % n_idle == 0:
+            helicity_modulus.append((T, compute_helicity_modulus(N, angles, T)))
             angles = np.zeros(shape = [n_idle, N, N])
             T = T + T_step
 
@@ -178,9 +181,9 @@ def simulate(N, T_init, T_end, T_step, n_idle):
 if __name__ == '__main__':
     #   Default simulation parameters
     N = 40
-    T_init = 0.6
-    T_end = 1.7
-    T_step = 0.01
+    T_init = 0.1
+    T_end = 4
+    T_step = 0.1
     n_idle = 50
 
     helicity_modulus = simulate(N, T_init, T_end, T_step, n_idle)
