2423829b9dcffdd43b7e244b4d186e59ed662aab
andrenmelo <andre.nascimento.melo@gmail.com>
2016-04-09 17:50:23 +0200

    xy part of report

--- batch_xy.py
+++ batch_xy.py
@@ -2,11 +2,11 @@ from xy_hk import simulate
 import pickle
 
 #   Default simulation parameters
-N = 40
+N = 100
 T_init = 0.7
 T_end = 1.6
 T_step = 0.1
-n_idle = 5000
+n_idle = 1000
 
 helicity_modulus = simulate(N, T_init, T_end, T_step, n_idle)
 pickle.dump(helicity_modulus, open('batch_xy/' + str(N),'wb'))
